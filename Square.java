d3d7ed0df8d7b9674d9a496698e85887e0082ae6
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-02-27 15:54:20 +0000

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,4 +1,5 @@
 import swiftbot.*;
+import java.io.*;
 
 public class square {
 
@@ -8,26 +9,45 @@ public class square {
         this.API = api;
     }
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RED = "\u001B[31m";
+
     public void drawSquare(int side) {
+        // checks if the sides are between 15 and 85 or not
+        if (side > 15 && side < 85) {
 
-        int toMove = side * 10;
+            System.out.println(" ");
+            System.out.println(ANSI_GREEN + "<< Swiftbot Is Drawing a Square >>" + ANSI_RESET);
 
-        for (int i = 0; i < 4; i++) {
+            int toMove = side * 10; // calculates the time the swftbot should move for to draw the side
 
-            API.move(100, 100, toMove);
-            API.move(0, 0, 1000);
-            // turns the swiftbot by 90 degrees exactly 3 times to complete the square
-            if (i < 3) {
-                API.move(100, -100, 400); // 400 seconds is 90 degrees
+            for (int i = 0; i < 4; i++) {
+                API.move(100, 100, toMove);
+                API.move(0, 0, 1000);
+
+                // turns the swiftbot by 90 degrees exactly 3 times to complete the square
+                if (i < 3) {
+                    API.move(100, -100, 400); // 400 seconds is 90 degrees
+                }
+
+                // sets the underlight to green when the drawing is finished
+                if (i == 3) {
+                    int[] colourToLightUp = { 0, 0, 255 };
+                    API.fillUnderlights(colourToLightUp);
+                    API.disableUnderlights();
+
+                    // calculates and prints the area of the square
+                    int area = side * side;
+                    System.out.println(ANSI_GREEN + "Area of this square is " + area + "cm sq" + ANSI_RESET);
+                    System.out.println(" ");
+                }
             }
-
-            // sets the underlight to green when the drawing is finished
-            if (i == 3) {
-                int[] colourToLightUp = { 0, 0, 255 };
-                API.fillUnderlights(colourToLightUp);
-            }
+        } else {
+            System.out.println(" ");
+            System.out.println(ANSI_RED + "The Sides Should Be Between 15 and 85" + ANSI_RESET);
+            System.out.println(" ");
         }
-        System.out.println("swiftbot is drawing a square");
 
     }
 
