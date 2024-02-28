63f454f9b3670be437e7144bcde5450d91c71580
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-02-28 19:27:49 +0000

    Update Square.java

--- Square.java
+++ Square.java
@@ -5,52 +5,46 @@ public class Square {
 
     private SwiftBotAPI API;
 
+    // Constructor to initialize Square object with SwiftBotAPI
     public Square(SwiftBotAPI api) {
         this.API = api;
     }
 
+    // ANSI color codes for colored output
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RED = "\u001B[31m";
 
+    // Method to draw a square with given side length
     public void drawSquare(int side) {
         try {
-            // checks if the sides are between 15 and 85 or not
-            if (side > 15 && side < 85) {
+            System.out.println(" ");
+            // Display message indicating square drawing has started
+            System.out.println(ANSI_GREEN + "<< Swiftbot Is Drawing a Square >>" + ANSI_RESET);
 
-                System.out.println(" ");
-                System.out.println(ANSI_GREEN + "<< Swiftbot Is Drawing a Square >>" + ANSI_RESET);
+            // calculates the time the swiftbot should move to draw each side of the square
+            int toMove = side * 10;
 
-                // calculates the time the swiftbot should move for to draw the side
-                int toMove = side * 80;
+            // Loop to draw each side of the square
+            for (int i = 0; i < 4; i++) {
+                API.move(100, 100, toMove); // Move the swiftbot to draw the side
+                API.move(0, 0, 1000); // Pause after drawing the side
 
-                for (int i = 0; i < 4; i++) {
-                    API.move(100, 100, toMove);
-                    API.move(0, 0, 1000);
-
-                    // turns the swiftbot by 90 degrees exactly 3 times to complete the square
-                    if (i < 3) {
-                        API.move(100, -100, 400); // 400 seconds is 90 degrees
-                    }
-
-                    // sets the underlight to green when the drawing is finished
-                    if (i == 3) {
-                        int[] colourToLightUp = { 0, 0, 255 };
-                        API.fillUnderlights(colourToLightUp);
-                        API.disableUnderlights();
-
-                        // calculates and prints the area of the square
-                        int areaSquare = side * side;
-                        System.out.println(ANSI_GREEN + "Area of this square is " + areaSquare + "cm sq" + ANSI_RESET);
-                        System.out.println(" ");
-                    }
+                // Turns the swiftbot by 90 degrees exactly 3 times to complete the square
+                if (i < 3) {
+                    API.move(100, -100, 400); // 400 milliseconds is 90 degrees turn
                 }
-            } else {
-                System.out.println(" ");
-                System.out.println(ANSI_RED + "The Sides Should Be Between 15 and 85" + ANSI_RESET);
-                System.out.println(" ");
+
+                // Sets the underlight to green when the drawing is finished
+                if (i == 3) {
+                    int[] colourToLightUp = { 0, 0, 255 };
+                    API.fillUnderlights(colourToLightUp);
+                    API.disableUnderlights();
+                }
+
             }
         } catch (Exception e) {
+            // Display error message if any exception occurs during drawing
             System.out.println(ANSI_RED + "Error Occurred While Drawing Square: " + e.getMessage() + ANSI_RESET);
         }
     }
