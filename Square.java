4c3522244dd98acbbfcb057aafb2ab2ebf185ce0
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-02-29 19:32:18 +0000

    Update Square.java

--- Square.java
+++ Square.java
@@ -5,7 +5,7 @@ public class Square {
 
     private SwiftBotAPI API;
 
-    // Constructor to initialize Square object with SwiftBotAPI
+    // constructor to initialize Square object with SwiftBotAPI
     public Square(SwiftBotAPI api) {
         this.API = api;
     }
@@ -15,22 +15,21 @@ public class Square {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RED = "\u001B[31m";
 
-    // Method to draw a square with given side length
+    // method to draw a square with given side length
     public void drawSquare(int side) {
         try {
             System.out.println(" ");
-            // Display message indicating square drawing has started
             System.out.println(ANSI_GREEN + "<< Swiftbot Is Drawing a Square >>" + ANSI_RESET);
 
             // calculates the time the swiftbot should move to draw each side of the square
-            int toMove = side * 10;
+            int toMove = side * 20;
 
-            // Loop to draw each side of the square
+            // loop to draw each side of the square
             for (int i = 0; i < 4; i++) {
-                API.move(100, 100, toMove); // Move the swiftbot to draw the side
-                API.move(0, 0, 1000); // Pause after drawing the side
+                API.move(50, 50, toMove);
+                API.move(0, 0, 1000); // pauses after drawing the side
 
-                // Turns the swiftbot by 90 degrees exactly 3 times to complete the square
+                // turns the swiftbot by 90 degrees exactly 3 times to complete the square
                 if (i < 3) {
                     API.move(100, -100, 400); // 400 milliseconds is 90 degrees turn
                 }
@@ -44,7 +43,6 @@ public class Square {
 
             }
         } catch (Exception e) {
-            // Display error message if any exception occurs during drawing
             System.out.println(ANSI_RED + "Error Occurred While Drawing Square: " + e.getMessage() + ANSI_RESET);
         }
     }
