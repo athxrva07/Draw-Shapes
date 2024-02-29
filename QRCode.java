58429dd1c1cc872e4a24e36b1092be2ed34887fb
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-02-29 19:36:21 +0000

    Update QRCode.java

--- QRCode.java
+++ QRCode.java
@@ -2,65 +2,70 @@ import swiftbot.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-public class qrCode {
+public class QRCode {
 
+    // ANSI color codes for colored output
     final String ANSI_RED = "\u001B[31m";
     final String ANSI_RESET = "\u001B[0m";
 
+    
     private SwiftBotAPI API;
 
-    public qrCode(SwiftBotAPI api) {
+    // initializes the QRCode object with a SwiftBotAPI instance
+    public QRCode(SwiftBotAPI api) {
         this.API = api;
     }
 
-    public int side;
-    public int side1;
-    public int side2;
-    public int side3;
-    public String shape;
+    // Properties to store information about the QR code
+    public int side;     // side length for square
+    public int side1;    // first side length for triangle
+    public int side2;    // second side length for triangle
+    public int side3;    // third side length for triangle
+    public String shape; 
 
+    // method to decode the QR code
     public void decodeQR() {
 
-        ArrayList<String> partsList = new ArrayList<>();
+        // ArrayList to store the decoded text
+        ArrayList<String> decodedInfo = new ArrayList<>();
 
         try {
-            // SwiftBotAPI API = new SwiftBotAPI();
             BufferedImage img = API.getQRImage();
 
             String decodedText = API.decodeQRImage(img);
 
             if (!decodedText.isEmpty()) {
-                // Split the decoded text at the space character (" ")
+                // splits the decoded text
                 String[] parts = decodedText.split(" ");
 
-                // Add the parts to the ArrayList
+                // adds the decoded text to the ArrayList
                 for (String part : parts) {
-                    partsList.add(part);
+                    decodedInfo.add(part);
                 }
 
-                // Check if the first element is "S" and print "Square"
-                if (partsList.get(0).equals("S")) {
-
+                if (decodedInfo.get(0).equals("S")) {
+                    
                     String decodedShape = "S";
                     shape = decodedShape;
-                    String s = partsList.get(1);
-                    int squareSide = Integer.parseInt(s.replaceAll("\\D", ""));
-                    side = squareSide;
-                } else if (partsList.get(0).equals("T")) {
 
+                    side = Integer.parseInt(decodedInfo.get(1));
+                } 
+                else if (decodedInfo.get(0).equals("T")) {
+                    
                     String decodedShape = "T";
                     shape = decodedShape;
 
-                    side1 = Integer.parseInt(partsList.get(1));
-                    side2 = Integer.parseInt(partsList.get(2));
-                    side3 = Integer.parseInt(partsList.get(3));
-
-                } else {
+                    side1 = Integer.parseInt(decodedInfo.get(1));
+                    side2 = Integer.parseInt(decodedInfo.get(2));
+                    side3 = Integer.parseInt(decodedInfo.get(3));
+                } 
+                else {
                     System.out.println(" ");
                     System.out.println(ANSI_RED + "No Shape Found");
                     System.out.println(" ");
                 }
-            } else {
+            } 
+            else {
                 System.out.println(" ");
                 System.out.println(ANSI_RED + "No QR Code Found" + ANSI_RESET);
                 System.out.println(" ");
